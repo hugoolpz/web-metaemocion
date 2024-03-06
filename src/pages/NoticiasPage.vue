90d580dc3cf9b63294851e118642c733721a26cc
hugoolpz <hugolopezfernandez13@gmail.com>
2024-03-06 14:57:21 +0100

    Tarjeta de vídeo

--- src/pages/NoticiasPage.vue
+++ src/pages/NoticiasPage.vue
@@ -2,6 +2,7 @@
 import { ref } from "vue";
 
 const slide = ref(1)
+const fullscreen = ref(false)
 </script>
 
 <template>
@@ -18,18 +19,43 @@ const slide = ref(1)
           <q-carousel
             animated
             v-model="slide"
+            v-model:fullscreen="fullscreen"
             arrows
-            navigation
             infinite
             class="shadow-2 rounded-border"
             control-color="me-rojo-claro"
             autoplay
-          >
-            <q-carousel-slide :name="1" :img-src="$t('/foto-noticia-logo.jpg')" />
+            thumbnails>
+            <q-carousel-slide :name="1" :img-src="$t('/foto-noticia-logo.jpg')"/>
             <q-carousel-slide :name="2" :img-src="$t('/foto-noticia-logo2.jpg')" />
             <q-carousel-slide :name="3" :img-src="$t('/foto-noticia-logo3.jpg')" />
-            <q-carousel-slide :name="4" :img-src="$t('/foto-noticia-logo4.jpg')" />
+
+            <template v-slot:control>
+              <q-carousel-control
+                position="bottom-right"
+                :offset="[18, 18]"
+              >
+                <q-btn
+                  push round dense color="white" text-color="me-rojo-claro"
+                  :icon="fullscreen ? 'fullscreen_exit' : 'fullscreen'"
+                  @click="fullscreen = !fullscreen"
+                />
+              </q-carousel-control>
+            </template>
           </q-carousel>
+        </q-card>
+
+        <q-card class="tarjeta-noticia q-my-md">
+          <q-card-section class="bg-me-negro text-white text-h5 text-weight-bold">
+            ¡Hemos hecho un primer trailer para Met@emoción!
+          </q-card-section>
+          <q-card-section class="text-justify">
+            Podemos mostraros un primer sneak peek de nuestro gran proyecto, ¡esperamos que os guste! Podeis verlo aquí:
+          </q-card-section>
+          <q-video
+            :ratio="16/9"
+            src="https://www.youtube.com/embed/k3_tw44QsZQ?rel=0"
+          />
         </q-card>
       </div>
     </div>
